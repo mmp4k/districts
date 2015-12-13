48b326ebdfa6d03269c69f46b63606978a4f939a
mmp4k <marcin@pilsniak.pl>
2015-12-13 03:24:53 +0100

    user bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,7 +1,7 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new mmp\rjpBundle\mmpRjpBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new mmp\UserBundle\mmpUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
